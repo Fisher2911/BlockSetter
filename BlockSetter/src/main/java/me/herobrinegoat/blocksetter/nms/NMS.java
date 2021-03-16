8a86b812d6bc35769927a5e4bea12310d27eeb97
dkim19375 <65050767+dkim19375@users.noreply.github.com>
2021-03-16 09:45:55 -0400

    Delete BlockSetter directory

--- BlockSetter/src/main/java/me/herobrinegoat/blocksetter/nms/NMS.java
+++
@@ -1,9 +0,0 @@
-package me.herobrinegoat.blocksetter.nms;
-
-import org.bukkit.Material;
-
-public interface NMS {
-
-    void setBlockFast(org.bukkit.World world, int x, int y, int z, Material material, byte data, boolean applyPhysics);
-
-}
